f10aadfcbdd026aeaec318196ca5eaba353470f3
ypujara <ypujara@purdue.edu>
2016-08-06 01:04:05 -0700

    loll

---
+++ js/aboutController.js
@@ -0,0 +1,16 @@
+(function (angular) {
+
+        var myApp = angular.module('myApp');
+        myApp.controller('aboutController', function ($scope, $location) {
+                console.log("lol");
+                var vm = this;
+                vm.message = 'this is the about page';
+                console.log("working");
+
+                vm.changeView() = function() {
+                        $location.path("/");
+                }
+
+        });
+
+})(angular);
