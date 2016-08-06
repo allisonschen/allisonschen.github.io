4612e66310daadfb97b75ba7f32f10fcb2d8bb0b
ypujara <ypujara@purdue.edu>
2016-08-06 00:54:17 -0700

    lol

--- js/homeController.js
+++ js/homeController.js
@@ -1,6 +1,6 @@
 (function (angular) {
 
-        var myApp = angular.module('homeController', []);
+        var myApp = angular.module('myApp');
         myApp.controller('homeController', function ($scope) {
                 console.log("lol");
                 vm = $scope;
