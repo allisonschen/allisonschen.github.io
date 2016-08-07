743852796775a5f3a93983c772fcc57bc68d34fb
ypujara <ypujara@purdue.edu>
2016-08-06 17:36:11 -0700

    testing view changer?

--- js/mainController.js
+++ js/mainController.js
@@ -3,6 +3,7 @@
     var myApp = angular.module('myApp');
     myApp.controller('aboutController', function ($scope, $location) {
         var vm = this;
+        vm.message = "hello";
         vm.aboutView = function () {
             $location.path("/about");
         }
