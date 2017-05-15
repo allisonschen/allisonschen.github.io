e436a4f8bd7343aa679b62e5031f6aa463cc3002
ypujara <ypujara@purdue.edu>
2017-05-14 23:28:54 -0700

    lmao

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 (function (angular) {
-    var myApp = angular.module('myApp', ['ui.bootstrap','ngRoute']);
-    myApp.config(function ($httpProvider,$routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'pages/home.html',
-                controller: 'homeController',
-                controllerAs: 'homeVm'
-            })
+  var myApp = angular.module('myApp', ['ui.bootstrap','ngRoute']);
+  myApp.config(function ($httpProvider,$routeProvider) {
+    $routeProvider
+    .when('/', {
+      templateUrl: 'pages/home.html',
+      controller: 'homeController',
+      controllerAs: 'homeVm'
+    })
 
-            .when('/about', {
-                templateUrl: 'pages/about.html',
-                controller: 'aboutController',
-                controllerAs: 'aboutVm'
-            })
-            .otherwise({
-                templateUrl: 'pages/home.html',
-                controller: 'homeController',
-                controllerAs: 'homeVm'
-            });
-	$httpProvider.defaults.withCredentials = true;
+    .when('/about', {
+      templateUrl: 'pages/about.html',
+      controller: 'aboutController',
+      controllerAs: 'aboutVm'
+    })
+    .otherwise({
+      templateUrl: 'pages/home.html',
+      controller: 'homeController',
+      controllerAs: 'homeVm'
     });
+    $httpProvider.defaults.withCredentials = true;
+  });
 
 })(angular);
