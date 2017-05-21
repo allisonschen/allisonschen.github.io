f8790e428713eda34077d4ea791249ed394b244d
ypujara <ypujara@purdue.edu>
2017-05-21 00:55:37 -0700

    lol

--- js/homeController.js
+++ js/homeController.js
@@ -18,7 +18,7 @@
       headers: {
 	"Access-Control-Allow-Headers" : "Origin,Content-Type,Authorization",
 	"Access-Control-Allow-Methods" : "GET",
-	"Access-Control-Allow-Origin" : "*,https://iworkwithmonkeys.com,allisonschen.github.io/",
+	"Access-Control-Allow-Origin" : "*",
 	"Access-Control-Allow-Credentials" : "true"
       }
     }).then(function(response){
