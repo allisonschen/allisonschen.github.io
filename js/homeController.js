e758f7791a83226813cb0a931d60b4761ff25f33
ypujara <ypujara@purdue.edu>
2017-05-22 22:41:16 -0700

    lmaozedong

--- js/homeController.js
+++ js/homeController.js
@@ -66,6 +66,7 @@
       }).then(function(response){
 	console.log(response);
 	angular.forEach(response.data.artists.items, function(item) {
+	  console.log(item);
 	  vm.results.push(item);
 	});
       });
