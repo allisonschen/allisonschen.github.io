f0cd93af153e0183a77a344b85aeb1fdb0ac6c6f
ypujara <ypujara@purdue.edu>
2017-05-23 21:05:46 -0700

    lmaozedong

--- js/homeController.js
+++ js/homeController.js
@@ -58,7 +58,8 @@
       return $http.get('https://api.spotify.com/v1/search', {
 	params: {
 	  q: val,
-	  type: 'artist'
+	  type: 'artist',
+	  limit: '50'
 	}
       }).then(function(response){
 	console.log(response);
