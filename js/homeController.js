59a0203071e08e4ab5d963dd3b5d51684db2dfd6
ypujara <ypujara@purdue.edu>
2017-05-31 10:12:57 +0530

    lmaozedong

--- js/homeController.js
+++ js/homeController.js
@@ -67,7 +67,8 @@
       console.log('user authenticated');
     },function error(response) {
       console.log($location.hash());
-      console.log($location.hash().split('/[&\=]/'));
+      console.log($location.hash().split('&')[0].split('=')[1]);
+      console.log($location.hash().split('/[&=]/'));
       console.log(response);
     });
     vm.search = function(val)  {
