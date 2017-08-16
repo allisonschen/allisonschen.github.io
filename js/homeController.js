b4b472eb503f41078092857220a5a4fe17107645
ypujara <ypujara@purdue.edu>
2017-08-15 21:20:33 -0700

    wesley is a feggit

--- js/homeController.js
+++ js/homeController.js
@@ -74,108 +74,110 @@
 	      "Content-Type" : "application/json"
 	    },
 	    params: {
-	     uris:  vm.urilist,
+	      uris:  vm.urilist,
 	    }
 	  });
 	});
       });
     };
-      vm.removeSelection = function(val, i) {
-	console.log(val,i);
-	console.log("removing");
+    vm.removeSelection = function(val, i) {
+      console.log(val,i);
+      console.log("removing");
+    }
+    vm.attrs = [{
+      "name": "Dance",
+      "m" : "danceability",
+      "val": 0.0
+    },{
+      "name" : "Akewstic",
+      "m" : "acousticness",
+      "val": 0.0
+    },{
+      "name" : "Instrooment",
+      "m" : "instrumentalness",
+      "val": 0
+    },{
+      "name": "Loud",
+      "m" : "loudness",
+      "val" : 0,
+    },{
+      "name" : "Energy",
+      "m" : "energy",
+      "val": 0 
+    },{ 
+      "name" : "Popular",
+      "m" : "popularity",
+      "val" : 0
+    },{
+      "name" : "Vocal",
+      "m" : "speechiness",
+      "val" : 0 
+    },{
+      "name" :"Happy",
+      "m" : "valence",
+      "val" : 0
+    }];
+    vm.finalj.attrs=vm.attrs;
+    $scope.artists=['lol','lolol','trololol','haha'];		
+    vm.token = [];
+    vm.artistSearch = "";
+    vm.onSelect = function(i,m,l) {
+      vm.finalj.seed_artists += i.uri.split(":")[2] + ",";
+      vm.imgs.unshift(i.img);
+      vm.artistSearch="";
+    }
+    $http.get('https://accounts.spotify.com/authorize', {
+      params: {
+	'client_id': '4543fb54a0694c1db55804cb18276c64',
+	'response_type': 'token',
+	'redirect_uri': 'http://iworkwithmonkeys.com',
+      },
+      headers: {
+	'Access-Control-Allow-Origin':'*'
       }
-      vm.attrs = [{
-	"name": "Dance",
-	"m" : "danceability",
-	"val": 0.0
-      },{
-	"name" : "Akewstic",
-	"m" : "acousticness",
-	"val": 0.0
-      },{
-	"name" : "Instrooment",
-	"m" : "instrumentalness",
-	"val": 0
-      },{
-	"name": "Loud",
-	"m" : "loudness",
-	"val" : 0,
-      },{
-	"name" : "Energy",
-	"m" : "energy",
-	"val": 0 
-      },{ 
-	"name" : "Popular",
-	"m" : "popularity",
-	"val" : 0
-      },{
-	"name" : "Vocal",
-	"m" : "speechiness",
-	"val" : 0 
-      },{
-	"name" :"Happy",
-	"m" : "valence",
-	"val" : 0
-      }];
-      vm.finalj.attrs=vm.attrs;
-      $scope.artists=['lol','lolol','trololol','haha'];		
-      vm.token = [];
-      vm.artistSearch = "";
-      vm.onSelect = function(i,m,l) {
-	vm.finalj.seed_artists += i.uri.split(":")[2] + ",";
-	vm.imgs.unshift(i.img);
-	vm.artistSearch="";
-      }
-      $http.get('https://accounts.spotify.com/authorize', {
-	params: {
-	  'client_id': '4543fb54a0694c1db55804cb18276c64',
-	  'response_type': 'token',
-	  'redirect_uri': 'http://iworkwithmonkeys.com',
-	},
+    }).then(function(response){
+      console.log(response);
+      console.log($location.hash());
+      console.log($location.hash().split('/[&=]/')[2])
+      console.log('user authenticated');
+    },function error(response) {
+      console.log(response);
+    });
+    vm.search = function(val)  {
+      console.log(val);
+      vm.results = [];
+      var auth = "Bearer " + accessToken;
+      return $http.get('https://api.spotify.com/v1/search', {
 	headers: {
-	  'Access-Control-Allow-Origin':'*'
+	  Authorization: auth
+	},
+	params: {
+	  q: val+'*',
+	  type: 'artist',
+	  limit: '50'
 	}
       }).then(function(response){
 	console.log(response);
-	console.log($location.hash());
-	console.log($location.hash().split('/[&=]/')[2])
-	console.log('user authenticated');
+	function compare(a,b) {
+	  return b.popularity - a.popularity;
+	}
+	response.data.artists.items.sort(compare);
+	return limitToFilter(angular.forEach(response.data.artists.items, function(item) {
+	  i = item.images[Object.keys(item.images)[item.images.length-1]];
+	  i != null ?  item.img=i.url  :  item.img="./images/DefaultArtist.png";
+	  return vm.results.push(item);
+	})
+	,5);
       },function error(response) {
 	console.log(response);
       });
-      vm.search = function(val)  {
-	console.log(val);
-	vm.results = [];
-	var auth = "Bearer " + accessToken;
-	return $http.get('https://api.spotify.com/v1/search', {
-	  headers: {
-	    Authorization: auth
-	  },
-	  params: {
-	    q: val+'*',
-	    type: 'artist',
-	    limit: '50'
-	  }
-	}).then(function(response){
-	  console.log(response);
-	  function compare(a,b) {
-	    return b.popularity - a.popularity;
-	  }
-	  response.data.artists.items.sort(compare);
-	  return limitToFilter(angular.forEach(response.data.artists.items, function(item) {
-	    i = item.images[Object.keys(item.images)[item.images.length-1]];
-	    i != null ?  item.img=i.url  :  item.img="./images/DefaultArtist.png";
-	    return vm.results.push(item);
-	  })
-	,5);
-	},function error(response) {
-	  console.log(response);
-	});
-      }
-      vm.changeView = function() {
-	console.log("clicked");
-	$location.path("/about");
-      }
+    }
+    vm.safetyurl = "https://open.spotify.com/embed?uri=spotify:user:" + homeVm.username + ":playlist:" + homeVm.playlistID;
+    vm.safety = $sce.trustAsResourceUrl(vm.safety); 
+    vm.changeView = function() {
+      console.log("clicked");
+      $location.path("/about");
+    }
 
-    });
-  })(angular);
+  });
+})(angular);
